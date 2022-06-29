63bf9879100e21deef233f416cf344860b3212b1
schuansk <schuansk@hotmail.com.br>
2022-06-29 20:27:23 -0300

    chore: add style pattern

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,11 @@
 import { DefaultTheme } from 'styled-components';
 
 import { colors } from './colors';
+import { constants } from './constants';
 
 const stylertTheme: DefaultTheme = {
   colors,
+  constants,
 };
 
 export default stylertTheme;
--- src/theme/styles.d.ts
+++ src/theme/styles.d.ts
@@ -1,9 +1,11 @@
 import 'styled-components';
 
 import { colors } from './colors';
+import { constants } from './constants';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: typeof colors;
+    constants: typeof constants;
   }
 }
