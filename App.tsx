69c5ac7be5b00f0ffccb91884ef7a5a7594fe204
schuansk <schuansk@hotmail.com.br>
2022-06-29 20:07:47 -0300

    feat: include theme

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StatusBar, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 import Routes from './src/routes';
+import stylertTheme from './src/theme';
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
-      <View style={{ flex: 1 }}>
-        <Routes />
-      </View>
+      <ThemeProvider theme={stylertTheme}>
+        <View style={{ flex: 1 }}>
+          <Routes />
+        </View>
+      </ThemeProvider>
     </NavigationContainer>
   );
 };
