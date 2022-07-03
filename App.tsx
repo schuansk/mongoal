a1b759d18d28b02e60103662fbafcc5becc92e1d
schuansk <schuansk@hotmail.com.br>
2022-07-03 18:37:11 -0300

    chore: include react-native-gesture-handler package

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
-import { StatusBar, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ThemeProvider } from 'styled-components';
 import Routes from './src/routes';
 import stylertTheme from './src/theme';
@@ -15,9 +16,9 @@ const App = () => {
         backgroundColor={stylertTheme.colors.purple_700}
       />
       <ThemeProvider theme={stylertTheme}>
-        <View style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
           <Routes />
-        </View>
+        </GestureHandlerRootView>
       </ThemeProvider>
     </NavigationContainer>
   );
