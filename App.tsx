780d6b60bcaf4bf5940820b1aed753791d4aa645
schuansk <schuansk@hotmail.com.br>
2022-07-03 11:33:02 -0300

    chore: include intl package

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
 import { StatusBar, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
