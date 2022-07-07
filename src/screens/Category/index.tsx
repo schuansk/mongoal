046ce24cbb8091a46bc55a88ba4b69c01e7631dc
schuansk <schuansk@hotmail.com.br>
2022-07-07 20:19:40 -0300

    feat: call modal on the category screen

--- src/screens/Category/index.tsx
+++ src/screens/Category/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { Text } from 'react-native';
 import CategoryList from '../../components/Category/List';
 import Header from '../../components/Header';
+import Modal from '../../components/Modal';
 import Navbar from '../../components/Navbar';
 import { Container, Content } from './styles';
 
 const Category: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Container>
-      <Header title="Categorias" />
+      <Header title="Categorias" onPress={toggleModal} />
       <Content>
         <CategoryList />
       </Content>
       <Navbar name="Category" />
+      <Modal toggleModal={toggleModal} isVisible={isOpen} height={0.25}>
+        <Text>Oi</Text>
+      </Modal>
     </Container>
   );
 };
