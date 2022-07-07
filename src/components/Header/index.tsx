2148ef00ee66fb84621bcc00d18f435bddc7f6e1
schuansk <schuansk@hotmail.com.br>
2022-07-07 20:19:07 -0300

    feat: include action in the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,15 @@ import { ActionButtonContainer, Container, Content, Title } from './styles';
 
 type HeaderProps = {
   title: string;
+  onPress(): void;
 };
-export const Header: React.FC<HeaderProps> = ({ title }) => {
+export const Header: React.FC<HeaderProps> = ({ title, onPress }) => {
   return (
     <Container>
       <Content>
         <Title>{title}</Title>
         <ActionButtonContainer>
-          <ActionButton light />
+          <ActionButton light onPress={onPress} />
         </ActionButtonContainer>
       </Content>
     </Container>
