7ce6a77f8b5366fa6fff2c7e6a9d43334278c3c3
schuansk <schuansk@hotmail.com.br>
2022-07-09 15:07:35 -0300

    refactor: return entered value

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,12 +3,16 @@ import { Container, TextInput } from './styles';
 
 type InputProps = {
   placeholder: string;
+  callback(categoryName: string): void;
 };
 
-const Input: React.FC<InputProps> = ({ placeholder }) => {
+const Input: React.FC<InputProps> = ({ placeholder, callback }) => {
   return (
     <Container>
-      <TextInput placeholder={placeholder} />
+      <TextInput
+        placeholder={placeholder}
+        onChangeText={value => callback(value)}
+      />
     </Container>
   );
 };
