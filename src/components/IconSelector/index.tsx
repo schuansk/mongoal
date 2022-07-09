d239a1d352d6893eafe79f510c3e945ba50be3e4
schuansk <schuansk@hotmail.com.br>
2022-07-09 14:33:49 -0300

    refactor: icon selector component

--- src/components/IconSelector/index.tsx
+++ src/components/IconSelector/index.tsx
@@ -1,8 +1,102 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
-import { Container } from './styles';
+import AvailableIcons from './AvailableIcons.json';
+import {
+  CheckIcon,
+  Container,
+  CurrentSelection,
+  CurrentSelectionTitle,
+  GoBack,
+  GoBackIcon,
+  OptionContainer,
+  OptionContent,
+  OptionIcon,
+  Options,
+  OptionsContent,
+  OptionsFooter,
+  OptionsSelectorCancel,
+  OptionsSelectorCancelTitle,
+  OptionsSelectorContainer,
+  OptionsSelectorHeader,
+  OptionsSelectorTitle,
+  SelectedIcon,
+} from './styles';
 
-const IconSelector: React.FC = () => {
-  return <Container />;
+type MaterialIconName = React.ComponentProps<typeof MaterialIcons>['name'];
+
+type IconSelectorProps = {
+  callback(icon: string): void;
+};
+
+const IconSelector: React.FC<IconSelectorProps> = ({ callback }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [icons, setIcons] = React.useState([]);
+  const [selectedIcon, setSelectedIcon] =
+    React.useState<MaterialIconName>('category');
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelectIcon = (icon: MaterialIconName) => {
+    setSelectedIcon(icon);
+    toggleModal();
+    callback(icon);
+  };
+
+  const keyExtractor = (icon: MaterialIconName) => icon;
+
+  const renderItem = ({ item: icon }) => (
+    <OptionContainer
+      selected={selectedIcon === icon}
+      onPress={() => handleSelectIcon(icon)}
+    >
+      <OptionContent>
+        <OptionIcon name={icon} />
+        {selectedIcon === icon && <CheckIcon />}
+      </OptionContent>
+    </OptionContainer>
+  );
+
+  const listFooterComponent = () => <OptionsFooter />;
+
+  React.useEffect(() => {
+    let isMounted = true;
+    if (isMounted) setIcons(AvailableIcons.icons);
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <Container>
+      <CurrentSelection onPress={toggleModal}>
+        <CurrentSelectionTitle>
+          <SelectedIcon name={selectedIcon} />
+        </CurrentSelectionTitle>
+      </CurrentSelection>
+      <OptionsSelectorContainer visible={isOpen} animationType="slide">
+        <OptionsSelectorHeader>
+          <GoBack onPress={toggleModal}>
+            <GoBackIcon />
+          </GoBack>
+          <OptionsSelectorTitle>Selecione um ícone</OptionsSelectorTitle>
+          <OptionsSelectorCancel onPress={toggleModal}>
+            <OptionsSelectorCancelTitle>Cancelar</OptionsSelectorCancelTitle>
+          </OptionsSelectorCancel>
+        </OptionsSelectorHeader>
+        <OptionsContent>
+          <Options
+            initialNumToRender={4}
+            data={icons}
+            renderItem={renderItem}
+            keyExtractor={keyExtractor}
+            ListFooterComponent={listFooterComponent}
+          />
+        </OptionsContent>
+      </OptionsSelectorContainer>
+    </Container>
+  );
 };
 
 export default IconSelector;
--- src/components/IconSelector/styles.ts
+++ src/components/IconSelector/styles.ts
@@ -1,3 +1,90 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+type OptionProps = {
+  selected: boolean;
+};
+
+export const Container = styled.View`
+  margin: 0 10px;
+  background: ${props => props.theme.colors.gray_100};
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+export const CurrentSelection = styled.TouchableOpacity``;
+
+export const CurrentSelectionTitle = styled.Text``;
+
+export const OptionsSelectorContainer = styled.Modal``;
+
+export const OptionsSelectorHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  padding: 0 15px 0 10px;
+`;
+
+export const GoBack = styled.TouchableOpacity.attrs(props => ({
+  activeOpacity: props.theme.constants.buttonActiveOpacity,
+}))``;
+
+export const GoBackIcon = styled(MaterialIcons).attrs(props => ({
+  name: 'chevron-left',
+  size: props.theme.constants.iconSize,
+  color: props.theme.colors.gray_600,
+}))``;
+
+export const OptionsSelectorTitle = styled.Text`
+  color: ${props => props.theme.colors.gray_600};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const OptionsSelectorCancel = styled.TouchableOpacity.attrs(props => ({
+  activeOpacity: props.theme.constants.buttonActiveOpacity,
+}))``;
+
+export const OptionsSelectorCancelTitle = styled.Text`
+  color: ${props => props.theme.colors.gray_600};
+`;
+
+export const OptionsContent = styled.View``;
+
+export const Options = styled.FlatList``;
+
+export const OptionContainer = styled.TouchableOpacity.attrs(props => ({
+  activeOpacity: props.theme.constants.buttonActiveOpacity,
+}))<OptionProps>`
+  padding: 0 15px;
+  background: ${props =>
+    props.selected ? props.theme.colors.gray_100 : props.theme.colors.white};
+`;
+
+export const OptionContent = styled.View`
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SelectedIcon = styled(MaterialIcons).attrs(props => ({
+  size: props.theme.constants.iconSize,
+  color: props.theme.colors.gray_600,
+}))``;
+
+export const OptionIcon = styled(MaterialIcons).attrs(props => ({
+  size: props.theme.constants.iconSize,
+  color: props.theme.colors.gray_600,
+}))``;
+
+export const CheckIcon = styled(MaterialIcons).attrs(props => ({
+  name: 'check',
+  size: props.theme.constants.iconSize,
+  color: props.theme.colors.purple_700,
+}))``;
+
+export const OptionsFooter = styled.View`
+  height: 50px;
+`;
