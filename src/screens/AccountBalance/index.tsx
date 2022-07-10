3f3540bcaf7b6dcf669ba016d5ff27ccead0d8e5
schuansk <schuansk@hotmail.com.br>
2022-07-10 17:07:23 -0300

    feat: include header on the accounts screen

--- src/screens/AccountBalance/index.tsx
+++ src/screens/AccountBalance/index.tsx
@@ -1,12 +1,19 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
 import React from 'react';
-import { Text } from 'react-native';
+import Header from '../../components/Header';
 import Navbar from '../../components/Navbar';
-import { Container } from './styles';
+import theme from '../../theme';
+import { Container, Content } from './styles';
 
 const AccountBalance: React.FC = () => {
   return (
     <Container>
-      <Text>Account Balance</Text>
+      <Header
+        title="Contas"
+        onPress={() => ({})}
+        background={[theme.colors.purple_700, theme.colors.orange_700]}
+      />
+      <Content />
       <Navbar name="AccountBalance" />
     </Container>
   );
--- src/screens/AccountBalance/styles.ts
+++ src/screens/AccountBalance/styles.ts
@@ -3,3 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const Content = styled.View`
+  flex: 1;
+`;
