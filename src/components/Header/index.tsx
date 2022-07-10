607927a3664135882100ecae8f23fceed837b8e3
schuansk <schuansk@hotmail.com.br>
2022-07-10 17:03:50 -0300

    feat: allow a personalized background plan

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import theme from '../../theme';
 import ActionButton from '../ActionButton';
 import { ActionButtonContainer, Container, Content, Title } from './styles';
 
 type HeaderProps = {
   title: string;
   onPress(): void;
+  background?: string[];
 };
-export const Header: React.FC<HeaderProps> = ({ title, onPress }) => {
+export const Header: React.FC<HeaderProps> = ({
+  title,
+  onPress,
+  background,
+}) => {
   return (
-    <Container>
+    <Container
+      colors={background || [theme.colors.purple_700, theme.colors.red_600]}
+    >
       <Content>
         <Title>{title}</Title>
         <ActionButtonContainer>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import styled from 'styled-components/native';
 
-export const Container = styled(LinearGradient).attrs(props => ({
-  colors: [props.theme.colors.purple_700, props.theme.colors.red_600],
+export const Container = styled(LinearGradient).attrs(() => ({
   start: { x: 0.0, y: 1.0 },
   end: { x: 1.0, y: 1.0 },
 }))`
