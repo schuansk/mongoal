a0d724f4df7af088e1d5b769c25f94ca2dc1a02e
schuansk <schuansk@hotmail.com.br>
2022-07-12 20:21:18 -0300

    feat: include account balance moda in screen

--- src/screens/AccountBalance/index.tsx
+++ src/screens/AccountBalance/index.tsx
@@ -1,17 +1,23 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 import React from 'react';
 import AccountBalanceListRender from '../../components/AccountBalance/List';
+import AccountBalanceModal from '../../components/AccountBalance/Modal';
 import Header from '../../components/Header';
 import Navbar from '../../components/Navbar';
 import theme from '../../theme';
 import { Container, Content } from './styles';
 
 const AccountBalance: React.FC = () => {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleModal = () => {
+    setIsVisible(!isVisible);
+  };
   return (
     <Container>
       <Header
         title="Contas"
-        onPress={() => ({})}
+        onPress={toggleModal}
         background={[theme.colors.purple_700, theme.colors.orange_700]}
       />
       <Content>
@@ -21,6 +27,11 @@ const AccountBalance: React.FC = () => {
         />
       </Content>
       <Navbar name="AccountBalance" />
+      <AccountBalanceModal
+        toggleModal={toggleModal}
+        isVisible={isVisible}
+        accountId=""
+      />
     </Container>
   );
 };
