964f81286b6126b4ddc43cc0d5a9fba6f8f551d0
schuansk <schuansk@hotmail.com.br>
2022-07-13 20:13:54 -0300

    feat: allow to create a new account

--- src/components/AccountBalance/Modal/index.tsx
+++ src/components/AccountBalance/Modal/index.tsx
@@ -1,6 +1,9 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 import React from 'react';
 import ColorPicker from 'react-native-wheel-color-picker';
+import { database } from '../../../database';
+import AccountModel from '../../../database/models/accountModel';
+import theme from '../../../theme';
 import Input from '../../Input';
 import Modal from '../../Modal';
 import {
@@ -27,6 +30,39 @@ const AccountBalanceModal: React.FC<ModalProps> = ({
   const [accountName, setAccountName] = React.useState('');
   const colorRef = React.useRef<ColorPicker>(null);
 
+  const reset = React.useCallback(
+    (changeModal = true) => {
+      if (changeModal) toggleModal();
+      setAccountName('');
+      setSelectedColor('');
+    },
+    [toggleModal],
+  );
+
+  const handleAddNewCategory = React.useCallback(
+    async (name: string, color: string) => {
+      try {
+        await database.write(async () => {
+          await database.collections
+            .get<AccountModel>('accounts')
+            .create(account => {
+              account.name = name;
+              account.color = color;
+            });
+        });
+        reset(true);
+      } catch (error) {
+        console.log('error', error);
+      }
+    },
+    [reset],
+  );
+
+  const handleSubmit = React.useCallback(() => {
+    if (accountName === '') return;
+    handleAddNewCategory(accountName, selectedColor);
+  }, [accountName, handleAddNewCategory, selectedColor]);
+
   return (
     <Modal toggleModal={toggleModal} isVisible={isVisible} height={0.59}>
       <ModalTitle>{accountId ? 'Editar' : 'Criar nova'} conta</ModalTitle>
@@ -35,6 +71,7 @@ const AccountBalanceModal: React.FC<ModalProps> = ({
           <ColorPickerContainer>
             <ColorPicker
               ref={colorRef}
+              color={theme.colors.gray_100}
               thumbSize={40}
               sliderSize={20}
               noSnap
@@ -52,7 +89,7 @@ const AccountBalanceModal: React.FC<ModalProps> = ({
           />
         </ModalSection>
         <ModalSection>
-          <CreateAccountButton onPress={() => ({})}>
+          <CreateAccountButton onPress={() => handleSubmit()}>
             <ConfirmIcon />
           </CreateAccountButton>
         </ModalSection>
